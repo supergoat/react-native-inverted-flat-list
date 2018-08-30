68e5200f94b795e462f361ce57f63b2a102f52f8
super goat <p.nicolaou.13@gmail.com>
2018-08-30 12:29:30 +0100

    ✨ Added the main component

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+
+/** ============================================================================
+<InvertedFlatList />
+--------------------------------------------------------------------------------
+React native inverted flat list with the ability to use pull to refresh
+============================================================================= */
+class InvertedFlatList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    )
+  }
+}
+
+/* Export
+============================================================================= */
+export default InvertedFlatList;
